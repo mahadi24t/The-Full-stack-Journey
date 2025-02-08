0d1bb3c3769be4cff9589c633fea2dd3cbdf3a1f
mahadi24t <mahadi24t@outlook.com>
2025-02-08 15:52:43 +0600

    oop js

--- functionc.js
+++ functionc.js
@@ -1,16 +1,32 @@
-// constructor function
-
-function person(firstName, lastName,dob) {
+// Constructor function
+function person(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.dob = dob;
+    this.dob = new Date(dob);
 }
 
-// Instatiate object
+// Add methods to the prototype using regular functions
+person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
 
-const person1= new person('John', 'Doe', '4-3-1980');
-const person2= new person('Mary', 'Smith', '3-6-1980');
+person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+person.prototype.getAge = function() {
+    let today = new Date();
+    let birthYear = this.getBirthYear();
+    let age = today.getFullYear() - birthYear;
+    return age;
+}
+
+// Instantiate objects
+const person1 = new person('John', 'Doe', '4-3-1980');
+const person2 = new person('Mary', 'Smith', '3-6-1980');
 
 console.log(person1.firstName, person1.lastName);
 console.log(person2.firstName, person2.lastName);
-console.log(person1.dob);
+console.log(person1.dob.getFullYear(), person1.firstName);
+console.log(person2.getBirthYear(), person2.getFullName());
+console.log('Name: ',person1.getFullName(),'| Age:', person1.getAge());
