ee552dec1143ead2446c4c0deda2a536097a3239
mahadi24t <mahadi24t@outlook.com>
2025-02-09 14:17:22 +0600

    domork

---
+++ mai.js
@@ -0,0 +1,12 @@
+console.log(window);
+
+window.alert(1);
+
+console.log(document.getElementById('my-form'));
+
+console.log(document.querySelector('.container'));
+console.log(document.querySelector('h1'));
+
+//multiple elements
+
+console.log(document.querySelectorAll('.item'));
